b40b99b99d3f21a6814d297ab9fb405ae72ceb62
Martin <cerny.mrtn@gmail.com>
2018-11-19 15:17:47 +0800

    Move jest configuration to config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-iphone-x-helper|lottie-react-native|expo|react-native-maps|react-native-svg|react-native-branch|native-base-shoutem-theme|react-native-easy-grid|react-native-drawer|react-native-carousel-view|react-native-vector-icons|react-native-keyboard-aware-scroll-view|react-native-swiper|react-navigation|native-base|@expo|react-native-scrollable-tab-view|react-native-simple-modal)/)',
+  ],
+  setupFiles: [
+    '<rootDir>/jest/setup.js',
+  ],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+};
