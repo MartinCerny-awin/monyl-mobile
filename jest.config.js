747f4270213cc40c1392bcd736fe609c45c3a173
Martin <cerny.mrtn@gmail.com>
2018-11-23 01:32:28 +0800

    Collect test coverage from relevant sources

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-iphone-x-helper|lottie-react-native|expo|react-native-maps|react-native-svg|react-native-branch|native-base-shoutem-theme|react-native-easy-grid|react-native-drawer|react-native-carousel-view|react-native-vector-icons|react-native-keyboard-aware-scroll-view|react-native-swiper|react-navigation|native-base|@expo|react-native-scrollable-tab-view|react-native-simple-modal)/)',
   ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/theme/**',
+    '!src/locales/**',
+  ],
   setupFiles: [
     '<rootDir>/jest/setup.js',
   ],
